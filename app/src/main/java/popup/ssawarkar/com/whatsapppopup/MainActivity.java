d85f6188c0e70dc777f35e4fe268a9f977d5f819
Saurabh Sawarkar <ssawarkar@vmware.com>
2018-07-02 22:25:47 +0530

    Initial Commit

---
+++ app/src/main/java/popup/ssawarkar/com/whatsapppopup/MainActivity.java
@@ -0,0 +1,36 @@
+package popup.ssawarkar.com.whatsapppopup;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        showPopup();
+    }
+
+    private void showPopup() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.screen_popup, null, false);
+        LinearLayout view = findViewById(R.id.activity_main);
+        view.addView(layout);
+    }
+}
